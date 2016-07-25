13851b1c42fe67b2bbb7e32da1cfedeb5f34b2e5
Manish Munikar <munikarmanish@gmail.com>
2016-07-26 02:29:04 +0545

    logged in users can submit new bug report

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 from . import signals
-from .views import BugDetail, BugList
+from .views import BugAdd, BugDetail, BugList
 
 urlpatterns = [
     url(r'^$', BugList.as_view(), name='bugs_list'),
     url(r'^(?P<id>\d+)/', BugDetail.as_view(), name='bugs_detail'),
+    url(r'^add/$', BugAdd.as_view(), name='bugs_add'),
 ]
--- bugs/views.py
+++ bugs/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView
 
+from .forms import BugReportForm
 from .models import BugCategory, BugReport
 
 
@@ -9,6 +13,7 @@ class BugList(ListView):
     model = BugReport
     page_kwarg = 'page'
     paginate_by = 30
+    ordering = '-id'
     template_name = 'bugs/list.html'
 
 
@@ -18,3 +23,22 @@ class BugDetail(DetailView):
     slug_url_kwarg = 'id'
     template_name = 'bugs/detail.html'
     context_object_name = 'bug'
+
+
+class BugAdd(LoginRequiredMixin, CreateView):
+    model = BugReport
+    form_class = BugReportForm
+    template_name = 'bugs/form.html'
+
+    def get_success_url(self):
+        return reverse('bugs_detail', args=(self.object.id,))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(BugAdd, self).get_context_data(*args, **kwargs)
+        context['page_title'] = 'Submit New Bug Report'
+        return context
+
+    def form_valid(self, form):
+        bug = form.save(commit=False)
+        bug.submitter = self.request.user
+        return super(BugAdd, self).form_valid(form)
