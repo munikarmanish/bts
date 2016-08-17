4f981eec86b1aaa1b8f75f305b6ad4b64b548349
Manish Munikar <munikarmanish@gmail.com>
2016-08-17 21:26:58 +0545

    added a simple profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import include, url
 from django.contrib.auth.views import login, logout
 
 from . import signals
-from .views import Register
+from .views import ProfileView, Register
 
 urlpatterns = [
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
     url(r'^register/$', Register.as_view(), name='register'),
+    url(r'^user/(?P<username>[\w_-]+)/$', ProfileView.as_view(), name='profile')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.views.generic import FormView
+from django.contrib.auth.models import User
+from django.views.generic import DetailView, FormView
 
 from .forms import RegistrationForm
 
@@ -20,3 +21,11 @@ class Register(FormView):
         user = authenticate(username=username, password=password)
         login(self.request, user)
         return super(Register, self).form_valid(form)
+
+
+class ProfileView(DetailView):
+    model = User
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    template_name = 'accounts/profile.html'
+    context_object_name = 'user'
